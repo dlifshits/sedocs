e3458356130371a49a33aa229bc93f692413baea
Lifshits, Dmitry <dmitry.lifshits@intel.com>
2022-10-10 16:31:00 -0700

    add docs directory
    Signed-off-by: Lifshits, Dmitry <dmitry.lifshits@intel.com>

---
+++ docs/source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'sedocs'
+copyright = '2022, seteam'
+author = 'seteam'
+release = 'v.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc',
+]
+
+# Display todos by setting to True
+todo_include_todos = True
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+root_doc = 'index'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
